ed851d12ffebda66fdc228dd6c198bab5e0bd27e
YoussefProDev <youssef23468210@gmail.com>
2024-05-10 21:50:00 +0200

    finito per adesso

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import path from "path";
 import ip from "ip";
 import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 import { instrument } from "@socket.io/admin-ui";
 const address = ip.address();
 const app = express();
@@ -27,94 +28,71 @@ app.get("/", (req, res) => {
   res.sendFile(`${path.resolve()}/public/index.html`);
 });
 
-// let selctedCharacter = null;
-
-// app.post("/arena", (req, res) => {
-//   const character = req.body.charactr;
-//   // Ora puoi utilizzare il personaggio selezionato come preferisci
-//   console.log("Selected character:", character);
-//   selctedCharacter = character;
-//   // Ad esempio, puoi reindirizzare l'utente alla pagina dell'arena
-// });
-// Route per "arena.html"
-
-let rooms = {};
+let rooms = [];
 // Funzione per trovare una stanza disponibile o crearne una nuova
 function findOrCreateRoom() {
-  let availableRoom = false;
-  if (rooms) {
-    Object.keys(rooms).some((roomId) => {
-      const room = rooms[`${roomId}`];
-      if (room.length < 2) {
-        availableRoom = room;
-        return true;
-      }
-      return false;
-    });
-  }
-
-  if (!availableRoom) {
-    let newRoom = `room-1`;
-    if (rooms.keys) {
-      newRoom = `room-${Object.keys(rooms).length + 1}`;
+  let availableRoomId = false;
+  // if (rooms) {
+  rooms.some((room) => {
+    // console.log(room.players);
+    if (room.players.length < 2) {
+      availableRoomId = room.id;
+      // console.log("available ", availableRoomId);
+      return true;
     }
+    return false;
+  });
+  // }
 
-    rooms[newRoom] = [];
-    availableRoom = newRoom;
-  }
-
-  return availableRoom;
+  return availableRoomId;
 }
 
 // Evento di connessione al namespace "arena"
 let i = 0;
 io.on("connection", (socket) => {
-  if (i === 0) {
-    setInterval(() => {
-      console.log(socket.adapter.rooms);
-    }, 5000);
-    i++;
-  }
+  socket.on("readyToPlay", (username) => {
+    let players = [];
+    socket.data = {
+      username: username,
+    };
+    if (i === 0) {
+      setInterval(() => {
+        // console.log(rooms);
+        // console.log(io.of("/").adapter.rooms);
+        // console.log(io.sockets.adapter.rooms);
+      }, 5000);
+      i++;
+    }
+    let roomId = findOrCreateRoom();
 
-  // Trova o crea una stanza disponibile
-  const roomId = findOrCreateRoom();
-  socket.join(roomId);
-  console.log("room", roomId);
-  // cot rooms = io.sockets.adapter.t
-  // console.log("ciao", io.sockets.adapter.rooms.get(roomId));
-  // socket.adapter.rooms.get(room).add(socket.id);`
+    if (roomId !== false) {
+      const room = rooms.filter((ele) => (ele.id === roomId ? true : false))[0];
+      // console.log("ffedfew", indexRoom, rooms[indexRoom]);
+      let playersRoom = room.players;
+      players = playersRoom;
+      console.log(socket.data.username);
+      playersRoom.push(socket.data.username);
+    } else {
+      roomId = uuidv4();
+      rooms.push({ id: roomId, players: [socket.data.username] });
+      players.push(socket.data.username);
+      // console.log("ciao");
+    }
+    console.log("ciao io entro in", roomId);
+    socket.join(roomId);
+    if (io.of("/").adapter.rooms.get(roomId).size > 1) {
+      io.of("/").to(roomId).emit("startGame", players);
+    }
+    socket.on("attack", (attack) => {
+      io.of("/").to(roomId).except(socket.id).emit("recivedAttack", attack);
+    });
 
-  // socket.adapter.rooms[room].push(socket.id);
-
-  // Controlla se la stanza ha ora 2 giocatori per iniziare il gioco
-  // if (rooms[roomId].length === 2) {
-  //   socket.to(roomId).emit("startGame");
-  // } else {
-  //   socket.to(roomId).emit("waiting");
-  // }
-  // socket.on(roomId, () => {});
-
-  // Gestisci la disconnessione del giocatore
-  socket.on("disconnect", () => {
-    console.log("id ", roomId);
-    console.log(rooms[roomId]);
-    // const index = rooms[roomId].indexOf(socket.id);
-    // if (index !== -1) {
-    //   // Rimuovi il giocatore dalla stanza
-    //   rooms[roomId].splice(index, 1);
-    //   // Se la stanza è ora vuota, elimina la stanza
-    //   if (rooms[roomId].length === 0) {
-    //     delete rooms[roomId];
-    //   } else {
-    //     // Se c'è ancora un giocatore nella stanza, avvisa l'altro giocatore della disconnessione
-    //     const remainingPlayerId = rooms[roomId][0];
-    //     socket
-    //       .to(remainingPlayerId)
-    //       .emit(`opponentDisconnected${remainingPlayerId}`);
-    //   }
-    // }
+    // Gestisci la disconnessione del giocatore
+    socket.on("disconnect", () => {
+      console.log("ciao io esco da", roomId);
+      io.of("/").to(roomId).emit("opponentDisconnect");
+    });
   });
-  // });
 });
 
 server.listen(3000, () => {
--- public/script.js
+++ public/script.js
@@ -5,21 +5,168 @@ usernameInput = document.querySelector("#usernameInput");
 
 usernameInput.value = localStorage.getItem("username");
 let username = "";
+let SelectedCharacter = {
+  name: "",
+  id: "",
+  hp: 0,
+  power: 0,
+  speed: 0,
+};
 
 document.querySelector("#submit").addEventListener("click", function () {
   username = usernameInput.value;
   localStorage.setItem("username", username);
   document.querySelector("#step1").classList.add("d-none");
-  document.querySelector("#step3").classList.remove("d-none");
-});
-const form = document.querySelector("#characterForm");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const selectedCharacter = document.getElementById("character").value;
-  // location.href = "/arena";
-
-  socket.emit("readyToPlay", selectedCharacter);
+  document.querySelector("#step2").classList.remove("d-none");
+  document.querySelector("#username").textContent = username;
 });
 
 console.log("Connesso al server");
+
+console.log("Document ready");
+
+let SelectedEnemy = {
+  name: "",
+  id: "",
+  hp: 0,
+  power: 0,
+  speed: 0,
+};
+
+document.querySelectorAll(".selectionChr").forEach((el, i) => {
+  el.addEventListener("click", (btn) => {
+    let dataset = btn.target.dataset;
+    SelectedCharacter = dataset;
+
+    // if (SelectedCharacter.id == "") {
+    //   SelectedCharacter.id = dataset.id;
+    //   SelectedCharacter.hp = parseInt(dataset.hp);
+    //   SelectedCharacter.speed = parseInt(dataset.speed);
+    //   SelectedCharacter.power = parseInt(dataset.power);
+    //   SelectedCharacter.name = dataset.name;
+    //   console.log("SelectedCharacter", SelectedCharacter);
+    // } else {
+    //   SelectedEnemy.id = dataset.id;
+    //   SelectedEnemy.hp = parseInt(dataset.hp);
+    //   SelectedEnemy.speed = parseInt(dataset.speed);
+    //   SelectedEnemy.power = parseInt(dataset.power);
+    //   SelectedEnemy.name = dataset.name;
+    //   console.log("SelectedEnemy", SelectedEnemy);
+    document.querySelector("#step2").classList.add("d-none");
+    document.querySelector("#attesa").classList.remove("d-none");
+    socket.emit("readyToPlay", username);
+  });
+});
+const opponentName = document.getElementById("opponentName");
+const myName = document.getElementById("myName");
+socket.on("startGame", (players) => {
+  players.forEach((player) => {
+    if (player != username) {
+      opponentName.textContent = player;
+    }
+  });
+  myName.textContent = username;
+  document.querySelector("#attesa").classList.add("d-none");
+  document.querySelector("#step3").classList.remove("d-none");
+});
+
+socket.on("opponentDisconnect", () => {
+  console.log("opponent disconnected");
+  document.querySelector("#step3").classList.add("d-none");
+  document.querySelector("#abbandonato").classList.remove("d-none");
+  setTimeout(() => {
+    location.reload();
+  }, 5000);
+});
+
+socket.on("recivedAttack", (attacco) => {
+  document.querySelector("#attacco").textContent = attacco;
+});
+
+// document.querySelector("info").ad
+const buttons = document.querySelectorAll("#actions button");
+buttons.forEach((butt) => {
+  butt.addEventListener("click", (event) => {
+    buttons.forEach((b) => {
+      b.disabled = true;
+    });
+    // document.querySelector("#attacco").textContent = event.target.id;
+    socket.emit("attack", event.target.id);
+    setTimeout(() => {
+      buttons.forEach((b) => {
+        b.disabled = false;
+      });
+    }, 3000);
+  });
+});
+
+function checkVictory(attacco) {
+  if (game.actionP1 == "pugno") {
+    switch (game.actionP2) {
+      case "schiva":
+        alert("pareggio");
+        break;
+      case "calcio":
+        alert("pareggio");
+        break;
+      case "para":
+        SelectedEnemy.hp -= SelectedCharacter.power;
+        alert("vince p1");
+        break;
+    }
+  }
+  if (game.actionP1 == "schiva") {
+    switch (game.actionP2) {
+      case "pugno":
+        alert("pareggio");
+        break;
+      case "calcio":
+        SelectedCharacter.hp -= SelectedEnemy.speed;
+        alert("vince p2");
+        break;
+      case "para":
+        alert("pareggio");
+        break;
+    }
+  }
+  if (game.actionP1 == "calcio") {
+    switch (game.actionP2) {
+      case "schiva":
+        SelectedEnemy.hp -= SelectedCharacter.speed;
+        alert("vince p1");
+        break;
+      case "pugno":
+        alert("pareggio");
+        break;
+      case "para":
+        alert("pareggio");
+        break;
+    }
+  }
+  if (game.actionP1 == "para") {
+    switch (game.actionP2) {
+      case "schiva":
+        alert("pareggio");
+        break;
+      case "pugno":
+        SelectedCharacter.hp -= SelectedEnemy.power;
+        alert("vince p2");
+        break;
+      case "calcio":
+        alert("pareggio");
+        break;
+    }
+  }
+  console.log(SelectedCharacter);
+  console.log(SelectedEnemy);
+  if (game.actionP1 == game.actionP2) {
+    alert("pareggio vince l'amicizia");
+  }
+  if (SelectedCharacter.hp <= 0) {
+    alert("ha vinto p2");
+    location.reload();
+  } else if (SelectedEnemy.hp <= 0) {
+    alert("ha vinto p1");
+    location.reload();
+  }
+}
